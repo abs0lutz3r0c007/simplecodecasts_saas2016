b617de4f5db3e52ee99e0b8a6b9f166abb7c0c9b
Newton Chan <newton.k.chan@gmail.com>
2016-08-04 21:02:15 +0000

    Retyped users.js file

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-  // Watch for a form submission:
-  $("#form-submit-btn").click(function(event) {
+  //Watch for a form submission
+  $('#form-submission-btn').click(function(event) {
     event.preventDefault();
     $('input[type=submit]').prop('disabled', true);
     var error = false;
-    var ccNum = $('#card_number').val(),
-        cvcNum = $('#card_code').val(),
-        expMonth = $('#card_month').val(),
+    var ccNum = $('#card_number').val();
+        cvcNum = $('#card_code').val();
+        expMonth = $('#card_month').val();
         expYear = $('#card_year').val();
     if (!error) {
-      // Get the Stripe token:
+      //Get Stripe token
       Stripe.createToken({
         number: ccNum,
         cvc: cvcNum,
@@ -19,15 +19,15 @@ $(document).ready(function() {
       }, stripeResponseHandler);
     }
     return false;
-  }); // form submission
+  }); //form submission
   function stripeResponseHandler(status, response) {
-    // Get a reference to the form:
-    var f = $("#new_user");
-    // Get the token from the response:
+    //Get a reference to the form:
+    var f = $('#new_user');
+    //Get the token from the response
     var token = response.id;
-    // Add the token to the form:
+    //Add the token to the form
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-    // Submit the form:
-    f.get(0).submit(); 
+    //Submit the form
+    f.get(0).submit();
   }
 });
